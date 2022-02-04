812ca754186b65fc80859bf205dacd6d48dc07ff
Sebastian Vargas <vargaspizango1@gmail.com>
2022-02-04 14:11:19 -0500

    chore: add basic routing

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllComponent } from './all/all.component';
 import { FavsComponent } from './favs/favs.component';
-
-
+import { HomeComponent } from './home.component';
+import { PagesRoutingModule } from './pages-routing.module';
 
 @NgModule({
-  declarations: [
-    AllComponent,
-    FavsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AllComponent, FavsComponent, HomeComponent],
+  imports: [CommonModule, PagesRoutingModule],
+  exports: [HomeComponent],
 })
-export class PagesModule { }
+export class PagesModule {}
