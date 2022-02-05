82acd38f516a85b428e504ca4b1780f8d7427108
Sebastian Vargas <vargaspizango1@gmail.com>
2022-02-04 21:17:54 -0500

    feat: add headline card

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,10 +4,11 @@ import { AllComponent } from './all/all.component';
 import { FavsComponent } from './favs/favs.component';
 import { HomeComponent } from './home.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AllComponent, FavsComponent, HomeComponent],
-  imports: [CommonModule, PagesRoutingModule],
+  imports: [CommonModule, PagesRoutingModule, SharedModule],
   exports: [HomeComponent],
 })
 export class PagesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { HeadlineComponent } from './headline/headline.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, HeadlineComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, HeadlineComponent],
 })
 export class SharedModule {}
