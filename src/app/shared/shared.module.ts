268bcd499e087899fedc088542529a3f01643eec
Sebastian Vargas <vargaspizango1@gmail.com>
2022-02-06 17:35:56 -0500

    feat: add sidebar persistence
    The sidebar now receives the same state as well as the dropdown.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HeadlineComponent } from './headline/headline.component';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HeadlineComponent, TimeAgoPipe],
+  declarations: [HeaderComponent, HeadlineComponent, TimeAgoPipe, SidebarComponent],
   imports: [CommonModule],
   exports: [HeaderComponent, HeadlineComponent],
 })
